e888162291575e31b9ff6afda607d0edb877a779
Mark Scheuerell <scheuerl@uw.edu>
2024-01-18 15:45:14 -0800

    working on data munging

--- 01_make-plots.R
+++ 01_make-plots.R
@@ -1,8 +1,19 @@
 ## script to create plots for Columbia River spending evaluation
 
-#### load data ####
+#### setup ####
+
+library(dplyr)
+
+
+#### load & munge data ####
 
 ## get data file
 data_raw <- read.csv(here::here("data", "columbia_spending.csv"))
 
+returns <- data_raw %>%
+  select(year, ends_with("returns")) %>%
+  mutate(chin_returns = chin_returns / 1000,
+         coho_returns = coho_returns / 1000,
+         sock_returns = sock_returns / 1000,
+         sthd_returns = sthd_returns / 1000)
 
