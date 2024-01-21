da766f4ec3b55f72cdc27971b86c70392f632b88
Mark Scheuerell <scheuerl@uw.edu>
2024-01-21 07:46:57 -0800

    added plot of hatchery releases

--- R/01_make-plots.R
+++ R/01_make-plots.R
@@ -23,7 +23,7 @@ spending <- data_raw %>%
   select(year, spending) %>%
   mutate(cum_spending = cumsum(spending))
 
-## annual & cumulative spending
+## hatchery releases
 releases <- data_raw %>%
   select(year, ends_with("rel")) %>%
   mutate(chin_rel = chin_rel / 1e6,
@@ -57,10 +57,43 @@ legend(x = 1975, y = 1200, xjust = 0.3, yjust = 0.7,
 
 dev.off()
 
-## annual & cumulative spending
-
-
 
 ## annual & cumulative spending
 
- 
+png(file = here::here("presentation", "figs", "annual_spending.png"),
+    width = 8, height = 5, units = "in", res = 300)
+
+par(mai = c(0.9, 0.9, 0.1, 0.1),
+    omi = c(0.1, 0.1, 0.1, 0.1))
+matplot(spending[,"year"], spending[,-1],
+        type = "o", lty = "solid", pch = 16, lwd = 2,
+        col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
+        yaxt = "n",
+        xlab = "Year", ylab = "Returns (1000s)",
+        cex.axis = 1.2, cex.lab = 1.5)
+
+dev.off()
+
+
+## hatchery releases
+
+png(file = here::here("presentation", "figs", "releases.png"),
+    width = 8, height = 5, units = "in", res = 300)
+
+par(mai = c(0.9, 0.9, 0.1, 0.1),
+    omi = c(0.1, 0.1, 0.1, 0.1))
+matplot(releases[,"year"], releases[,-1],
+        type = "o", lty = "solid", pch = 16, lwd = 2,
+        col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
+        xlab = "Year", ylab = "Releases (millions)",
+        cex.axis = 1.2, cex.lab = 1.5)
+legend(x = 2010, y = 30, xjust = 0.5, yjust = 0.5,
+       legend = c("Chinook", "Coho", "Sockeye", "Steelhead"),
+       col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
+       text.col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
+       bty = "n", lty = "solid", pch = 16, lwd = 2,
+       y.intersp = 1.2)
+
+dev.off()
+
+
