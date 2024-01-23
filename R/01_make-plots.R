9ac3c5ee3a92bdbe259f728b3ec965bc2a225f0c
Mark Scheuerell <scheuerl@uw.edu>
2024-01-23 11:26:45 -0800

    increased font size on spending plots

--- R/01_make-plots.R
+++ R/01_make-plots.R
@@ -46,8 +46,8 @@ matplot(returns[,"year"], returns[,-1],
         col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
         yaxt = "n",
         xlab = "Year", ylab = "Returns (1000s)",
-        cex.axis = 1.2, cex.lab = 1.5)
-axis(side = 2, at = c(0, 400, 800, 1200), cex.axis = 1.2)
+        cex.axis = 1.4, cex.lab = 1.6)
+axis(side = 2, at = c(0, 400, 800, 1200), cex.axis = 1.4)
 legend(x = 1975, y = 1200, xjust = 0.3, yjust = 0.7,
        legend = c("Chinook", "Coho", "Sockeye", "Steelhead"),
        col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
@@ -70,7 +70,7 @@ plot(spending[,"year"], spending[,"spending"],
         type = "o", lty = "solid", pch = 16, lwd = 2,
         col = "#008837",
         xlab = "Year", ylab = "Spending (millions)",
-        cex.axis = 1.2, cex.lab = 1.5)
+        cex.axis = 1.4, cex.lab = 1.6)
 
 dev.off()
 
@@ -84,7 +84,7 @@ plot(spending[,"year"], spending[,"cum_spending"],
      type = "o", lty = "solid", pch = 16, lwd = 2,
      col = "#008837",
      xlab = "Year", ylab = "Cumulative spending (billions)",
-     cex.axis = 1.2, cex.lab = 1.5)
+     cex.axis = 1.4, cex.lab = 1.6)
 
 dev.off()
 
@@ -100,7 +100,7 @@ matplot(releases[,"year"], releases[,-1],
         type = "o", lty = "solid", pch = 16, lwd = 2,
         col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
         xlab = "Year", ylab = "Releases (millions)",
-        cex.axis = 1.2, cex.lab = 1.5)
+        cex.axis = 1.4, cex.lab = 1.6)
 legend(x = 2010, y = 30, xjust = 0.5, yjust = 0.5,
        legend = c("Chinook", "Coho", "Sockeye", "Steelhead"),
        col = c("#a6611a", "#2b83ba", "#ca0020", "#5e3c99"),
